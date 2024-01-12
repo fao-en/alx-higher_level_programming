273daefdeed9fb6ea7d8b8291bfbd4920b9dad93
fao-en <fennaama@outlook.com>
2024-01-12 07:19:28 -0800

    update models

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,11 +4,16 @@ from relationship_state import Base
 """
 python script that performs creates a States class based off of Base.
 """
+Base = declarative_base()
 
 
 class City(Base):
     """
-        The "City" class which inherits from "Base" class.
+The "City" class which inherits from "Base" class.
+Attributes:
+id (str): The city's id.
+name (sqlalchemy.Integer): The city's name.
+state_id (sqlalchemy.String): The city's state id.
     """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
