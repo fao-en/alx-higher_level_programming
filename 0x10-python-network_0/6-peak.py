d8f2fd82dc6cafb9ab1c4cbd3e291a529f371841
fao-en <fennaama@outlook.com>
2024-01-25 05:32:59 -0800

    6 task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,44 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers."""
+"""Finds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
+    """Finds a peak in the given list.
 
-    if list_of_integers is None or list_of_integers == []:
+    Args:
+        list_of_integers (list): List of unsorted integers.
+
+    Returns:
+        int: The peak element in the list.
+    """
+
+    # Check if the list is empty
+    if list_of_integers is None or not list_of_integers:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
+
+    # Initialize variables for binary search
+    low = 0
+    high = len(list_of_integers)
+    mid = (high - low) // 2 + low
     mid = int(mid)
-    if hi == 1:
+
+    # Base cases for lists of size 1 and 2
+    if high == 1:
         return list_of_integers[0]
-    if hi == 2:
+    if high == 2:
         return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
+
+    # Check if the middle element is a peak
+    if (
+        list_of_integers[mid] >= list_of_integers[mid - 1] and
+        list_of_integers[mid] >= list_of_integers[mid + 1]
+    ):
         return list_of_integers[mid]
+
+    # Recursively search in the right half if the right neighbor is larger
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
         return find_peak(list_of_integers[mid:])
+
+    # Recursively search in the left half if the left neighbor is larger
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
