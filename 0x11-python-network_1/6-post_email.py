af386e278bf69fd013088f223be5ac17cdf812d8
fao-en <fennaama@outlook.com>
2024-01-26 04:31:21 -0800

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a parameter
+"""Python script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and finally displays the body
+of the response.
 """
+
+import requests
 import sys
-import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    email = sys.argv[2]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+
+    print("Your email is:", email)
+    print(response.text)
